f743b9c84745a72f8183c9f1576c77a68070fad2
Asiapenguin <nicholaskong@live.ca>
2018-12-09 14:58:49 -0800

    check chain valid

--- dev/blockchain.ts
+++ dev/blockchain.ts
@@ -1,12 +1,14 @@
-import { Block } from './block';
-import { Transaction } from './transaction';
-import * as hash from 'hash.js';
+import { Block } from "./block";
+import { Transaction } from "./transaction";
+import * as hash from "hash.js";
 import uuid = require("uuid/v1");
 
 const currentNodeURL = process.argv[3];
+const GENESIS_NONCE = 100;
+const GENESIS_PREVHASH = "0";
+const GENESIS_HASH = "0";
 
 export class Blockchain {
-  
   chain: Array<Block>;
   pendingTransactions: Array<Transaction>;
   currentNodeURL: string;
@@ -16,16 +18,23 @@ export class Blockchain {
     this.chain = [];
     this.pendingTransactions = [];
 
-    this.currentNodeURL = currentNodeURL
+    this.currentNodeURL = currentNodeURL;
     this.networkNodesURL = [];
 
     // Genesis block
-    this.createNewBlock(100, '0', '0');
+    this.createNewBlock(GENESIS_NONCE, GENESIS_PREVHASH, GENESIS_HASH);
   }
 
   createNewBlock(nonce, prevHash, hash): Block {
     const index = this.chain.length;
-    const newBlock = new Block(index, Date.now(), this.pendingTransactions, nonce, hash, prevHash);
+    const newBlock = new Block(
+      index,
+      Date.now(),
+      this.pendingTransactions,
+      nonce,
+      hash,
+      prevHash
+    );
 
     this.pendingTransactions = [];
     this.chain.push(newBlock);
@@ -38,7 +47,9 @@ export class Blockchain {
   }
 
   createNewTransaction(amount, sender, receiver): Transaction {
-    const newUuid = uuid().split('-').join('');
+    const newUuid = uuid()
+      .split("-")
+      .join("");
     const newTransaction = new Transaction(newUuid, amount, sender, receiver);
 
     return newTransaction;
@@ -46,14 +57,18 @@ export class Blockchain {
 
   addTransactionToPendingTransactions(transaction: Transaction): number {
     this.pendingTransactions.push(transaction);
-    
+
     return this.getLastBlock().index + 1;
   }
 
   // SHA-256
   hashBlock(data: Block, nonce: number): string {
-    const dataString = data.prevHash + nonce.toString() + JSON.stringify(data.transactions);
-    const resultHash = hash.sha256().update(dataString).digest('hex');
+    const dataString =
+      data.prevHash + nonce.toString() + JSON.stringify(data.transactions);
+    const resultHash = hash
+      .sha256()
+      .update(dataString)
+      .digest("hex");
 
     return resultHash;
   }
@@ -62,8 +77,8 @@ export class Blockchain {
     // repeatedly hash until hash starts with 4 0's
     let nonce = 0;
     let newHash = this.hashBlock(data, nonce);
-    
-    while(!this.checkLegitimate(newHash)) {
+
+    while (!this.checkLegitimate(newHash)) {
       nonce++;
       newHash = this.hashBlock(data, nonce);
     }
@@ -73,6 +88,43 @@ export class Blockchain {
 
   private checkLegitimate(newHash: string) {
     const firstFourChar = newHash.substr(0, 4);
-    return firstFourChar === '0000';
+    return firstFourChar === "0000";
+  }
+
+  isChainValid(blockchain): boolean {
+    if (blockchain.chain) {
+      let valid = true;
+
+      for (let i = 1; i < blockchain.chain.length; i++) {
+        const currentBlock = blockchain.chain[i];
+        const prevBlock = blockchain.chain[i - 1];
+        const hash = this.hashBlock(prevBlock, currentBlock.nonce);
+
+        // check for order
+        if (currentBlock.prevHash !== prevBlock.hash) {
+          valid = false;
+        }
+
+        // check for data validity
+        if (!this.checkLegitimate(hash)) {
+          valid = false;
+        }
+      }
+
+      // check genesis block
+      const genesisBlock: Block = blockchain.chain[0];
+      if (
+        genesisBlock.nonce !== GENESIS_NONCE ||
+        genesisBlock.prevHash !== GENESIS_PREVHASH ||
+        genesisBlock.hash !== GENESIS_HASH ||
+        genesisBlock.transactions.length !== 0
+      ) {
+        valid = false;
+      }
+
+      return valid;
+    } else {
+      return false;
+    }
   }
 }
--- spec/blockchain.spec.ts
+++ spec/blockchain.spec.ts
@@ -7,33 +7,474 @@ import { Transaction } from "../dev/transaction";
 describe("BlockchainTest", () => {
   const blockchain = new Blockchain();
 
-  const prevHash =
-    "5D25DEC8523C8B3726E073A9946A";
-  const currentHash =
-    "47787110B5C621433BE3607B9B50";
+  const block1Hash = "47787110B5C621433BE3607B9B50";
 
   const transactions = [
     new Transaction("IUO9UASIU12", 100, "ADKFH123", "34HH123SAF"),
     new Transaction("9H2J3KH8JAS", 100, "PIOH14123", "HKLH123SDF")
   ];
 
+  const validTestChain = {
+    chain: [
+      {
+        index: 0,
+        timestamp: 1544395680414,
+        transactions: [],
+        nonce: 100,
+        hash: "0",
+        prevHash: "0"
+      },
+      {
+        index: 1,
+        timestamp: 1544395693151,
+        transactions: [
+          {
+            uuid: "7fdcddd0fc0411e89194edec8bbbb302",
+            amount: 10,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          }
+        ],
+        nonce: 4922,
+        hash:
+          "0000948c41f8bc463e1d058f48d7f28d6fd097f47978d0d247ba3472c2247990",
+        prevHash: "0"
+      },
+      {
+        index: 2,
+        timestamp: 1544395709026,
+        transactions: [
+          {
+            uuid: "82577ac0fc0411e89194edec8bbbb302",
+            amount: 12.5,
+            sender: "00",
+            receiver: "7abdd4d0fc0411e89194edec8bbbb302"
+          },
+          {
+            uuid: "8661df70fc0411e89194edec8bbbb302",
+            amount: 20,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "89b9e410fc0411e89194edec8bbbb302",
+            amount: 30,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          }
+        ],
+        nonce: 2505,
+        hash:
+          "0000fa51a16ea1eb829ce6b3cb6afcab516252603f30b04fcd4568fffa31a989",
+        prevHash:
+          "0000948c41f8bc463e1d058f48d7f28d6fd097f47978d0d247ba3472c2247990"
+      },
+      {
+        index: 3,
+        timestamp: 1544395723044,
+        transactions: [
+          {
+            uuid: "8bcc7060fc0411e89194edec8bbbb302",
+            amount: 12.5,
+            sender: "00",
+            receiver: "7abdd4d0fc0411e89194edec8bbbb302"
+          }
+        ],
+        nonce: 109613,
+        hash:
+          "0000133c759c5cf2d2719ec8c865d46757596998485a99a7801706d61562eabb",
+        prevHash:
+          "0000fa51a16ea1eb829ce6b3cb6afcab516252603f30b04fcd4568fffa31a989"
+      }
+    ],
+    pendingTransactions: [
+      {
+        uuid: "94274370fc0411e89194edec8bbbb302",
+        amount: 12.5,
+        sender: "00",
+        receiver: "7abdd4d0fc0411e89194edec8bbbb302"
+      }
+    ],
+    currentNodeURL: "http://localhost:3000",
+    networkNodesURL: []
+  };
+  // change hash of index 1 block
+  const invalidTestChainHash = {
+    chain: [
+      {
+        index: 0,
+        timestamp: new Date(1544391715951),
+        transactions: [],
+        nonce: 100,
+        hash: "0",
+        prevHash: "0"
+      },
+      {
+        index: 1,
+        timestamp: new Date(1544391775160),
+        transactions: [
+          {
+            uuid: "563929b0fbfb11e89c9f33144df52554",
+            amount: 10,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "5aa97c20fbfb11e89c9f33144df52554",
+            amount: 20,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "5cea6b70fbfb11e89c9f33144df52554",
+            amount: 30,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          }
+        ],
+        nonce: 4922,
+        hash: "0000948c41f8bc463e1d058f48d7f28d6fd097f47978d0d247ba3472c224",
+        prevHash: "0"
+      },
+      {
+        index: 2,
+        timestamp: new Date(1544391797732),
+        transactions: [
+          {
+            uuid: "6309a660fbfb11e89c9f33144df52554",
+            amount: 12.5,
+            sender: "00",
+            receiver: "3fbceff0fbfb11e89c9f33144df52554"
+          },
+          {
+            uuid: "6d061b80fbfb11e89c9f33144df52554",
+            amount: 40,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "6f4201c0fbfb11e89c9f33144df52554",
+            amount: 50,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          }
+        ],
+        nonce: 5047,
+        hash:
+          "0000413af8408ba728979ef2bf5cffabec8aafa24585e28a31458013a8730343",
+        prevHash:
+          "0000948c41f8bc463e1d058f48d7f28d6fd097f47978d0d247ba3472c2247990"
+      },
+      {
+        index: 3,
+        timestamp: new Date(1544391807184),
+        transactions: [
+          {
+            uuid: "707c2f70fbfb11e89c9f33144df52554",
+            amount: 12.5,
+            sender: "00",
+            receiver: "3fbceff0fbfb11e89c9f33144df52554"
+          }
+        ],
+        nonce: 3895,
+        hash:
+          "0000915b2fd28b780df1e137fb4684d041778e2d9ef91240108e459d9f9d178a",
+        prevHash:
+          "0000413af8408ba728979ef2bf5cffabec8aafa24585e28a31458013a8730343"
+      },
+      {
+        index: 4,
+        timestamp: new Date(1544391809765),
+        transactions: [
+          {
+            uuid: "761e9940fbfb11e89c9f33144df52554",
+            amount: 12.5,
+            sender: "00",
+            receiver: "3fbceff0fbfb11e89c9f33144df52554"
+          }
+        ],
+        nonce: 89503,
+        hash:
+          "00009b913d3dd0ef71afa7c2c96f73efc1ab23838672f8f124c77bdc5a2a9667",
+        prevHash:
+          "0000915b2fd28b780df1e137fb4684d041778e2d9ef91240108e459d9f9d178a"
+      }
+    ],
+    pendingTransactions: [
+      {
+        uuid: "77a86d90fbfb11e89c9f33144df52554",
+        amount: 12.5,
+        sender: "00",
+        receiver: "3fbceff0fbfb11e89c9f33144df52554"
+      }
+    ],
+    currentNodeURL: "http://localhost:3000",
+    networkNodesURL: []
+  };
+  // change transaction of index 1 block
+  const invalidTestChainTransaction = {
+    chain: [
+      {
+        index: 0,
+        timestamp: new Date(1544391715951),
+        transactions: [],
+        nonce: 100,
+        hash: "0",
+        prevHash: "0"
+      },
+      {
+        index: 1,
+        timestamp: new Date(1544391775160),
+        transactions: [
+          {
+            uuid: "563929b0fbfb11e89c9f33144df52554",
+            amount: 10,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "5aa97c20fbfb11e89c9f33144df52554",
+            amount: 20,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "5cea6b70fbfb11e89c9f33144df52554",
+            amount: 30,
+            sender: "ASDFA4214",
+            receiver: "36EYSDFASDFASDF"
+          }
+        ],
+        nonce: 4922,
+        hash: "0000948c41f8bc463e1d058f48d7f28d6fd097f47978d0d247ba3472c2247990",
+        prevHash: "0"
+      },
+      {
+        index: 2,
+        timestamp: new Date(1544391797732),
+        transactions: [
+          {
+            uuid: "6309a660fbfb11e89c9f33144df52554",
+            amount: 12.5,
+            sender: "00",
+            receiver: "3fbceff0fbfb11e89c9f33144df52554"
+          },
+          {
+            uuid: "6d061b80fbfb11e89c9f33144df52554",
+            amount: 40,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "6f4201c0fbfb11e89c9f33144df52554",
+            amount: 50,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          }
+        ],
+        nonce: 5047,
+        hash:
+          "0000413af8408ba728979ef2bf5cffabec8aafa24585e28a31458013a8730343",
+        prevHash:
+          "0000948c41f8bc463e1d058f48d7f28d6fd097f47978d0d247ba3472c2247990"
+      },
+      {
+        index: 3,
+        timestamp: new Date(1544391807184),
+        transactions: [
+          {
+            uuid: "707c2f70fbfb11e89c9f33144df52554",
+            amount: 12.5,
+            sender: "00",
+            receiver: "3fbceff0fbfb11e89c9f33144df52554"
+          }
+        ],
+        nonce: 3895,
+        hash:
+          "0000915b2fd28b780df1e137fb4684d041778e2d9ef91240108e459d9f9d178a",
+        prevHash:
+          "0000413af8408ba728979ef2bf5cffabec8aafa24585e28a31458013a8730343"
+      },
+      {
+        index: 4,
+        timestamp: new Date(1544391809765),
+        transactions: [
+          {
+            uuid: "761e9940fbfb11e89c9f33144df52554",
+            amount: 12.5,
+            sender: "00",
+            receiver: "3fbceff0fbfb11e89c9f33144df52554"
+          }
+        ],
+        nonce: 89503,
+        hash:
+          "00009b913d3dd0ef71afa7c2c96f73efc1ab23838672f8f124c77bdc5a2a9667",
+        prevHash:
+          "0000915b2fd28b780df1e137fb4684d041778e2d9ef91240108e459d9f9d178a"
+      }
+    ],
+    pendingTransactions: [
+      {
+        uuid: "77a86d90fbfb11e89c9f33144df52554",
+        amount: 12.5,
+        sender: "00",
+        receiver: "3fbceff0fbfb11e89c9f33144df52554"
+      }
+    ],
+    currentNodeURL: "http://localhost:3000",
+    networkNodesURL: []
+  };
+  // change nonce of genesis block
+  const invalidTestChainGenesis = {
+    chain: [
+      {
+        index: 0,
+        timestamp: new Date(1544391715951),
+        transactions: [],
+        nonce: 0,
+        hash: "0",
+        prevHash: "0"
+      },
+      {
+        index: 1,
+        timestamp: new Date(1544391775160),
+        transactions: [
+          {
+            uuid: "563929b0fbfb11e89c9f33144df52554",
+            amount: 10,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "5aa97c20fbfb11e89c9f33144df52554",
+            amount: 20,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "5cea6b70fbfb11e89c9f33144df52554",
+            amount: 30,
+            sender: "ASDFA4214",
+            receiver: "36EYSDFASDFASDF"
+          }
+        ],
+        nonce: 4922,
+        hash: "0000948c41f8bc463e1d058f48d7f28d6fd097f47978d0d247ba3472c2247990",
+        prevHash: "0"
+      },
+      {
+        index: 2,
+        timestamp: new Date(1544391797732),
+        transactions: [
+          {
+            uuid: "6309a660fbfb11e89c9f33144df52554",
+            amount: 12.5,
+            sender: "00",
+            receiver: "3fbceff0fbfb11e89c9f33144df52554"
+          },
+          {
+            uuid: "6d061b80fbfb11e89c9f33144df52554",
+            amount: 40,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          },
+          {
+            uuid: "6f4201c0fbfb11e89c9f33144df52554",
+            amount: 50,
+            sender: "ASDFA4214FASD",
+            receiver: "36EYSDFASDFASDF"
+          }
+        ],
+        nonce: 5047,
+        hash:
+          "0000413af8408ba728979ef2bf5cffabec8aafa24585e28a31458013a8730343",
+        prevHash:
+          "0000948c41f8bc463e1d058f48d7f28d6fd097f47978d0d247ba3472c2247990"
+      },
+      {
+        index: 3,
+        timestamp: new Date(1544391807184),
+        transactions: [
+          {
+            uuid: "707c2f70fbfb11e89c9f33144df52554",
+            amount: 12.5,
+            sender: "00",
+            receiver: "3fbceff0fbfb11e89c9f33144df52554"
+          }
+        ],
+        nonce: 3895,
+        hash:
+          "0000915b2fd28b780df1e137fb4684d041778e2d9ef91240108e459d9f9d178a",
+        prevHash:
+          "0000413af8408ba728979ef2bf5cffabec8aafa24585e28a31458013a8730343"
+      },
+      {
+        index: 4,
+        timestamp: new Date(1544391809765),
+        transactions: [
+          {
+            uuid: "761e9940fbfb11e89c9f33144df52554",
+            amount: 12.5,
+            sender: "00",
+            receiver: "3fbceff0fbfb11e89c9f33144df52554"
+          }
+        ],
+        nonce: 89503,
+        hash:
+          "00009b913d3dd0ef71afa7c2c96f73efc1ab23838672f8f124c77bdc5a2a9667",
+        prevHash:
+          "0000915b2fd28b780df1e137fb4684d041778e2d9ef91240108e459d9f9d178a"
+      }
+    ],
+    pendingTransactions: [
+      {
+        uuid: "77a86d90fbfb11e89c9f33144df52554",
+        amount: 12.5,
+        sender: "00",
+        receiver: "3fbceff0fbfb11e89c9f33144df52554"
+      }
+    ],
+    currentNodeURL: "http://localhost:3000",
+    networkNodesURL: []
+  };
+
   it("should create a new block", () => {
     expect(blockchain.chain.length).to.be.equal(1);
-    blockchain.createNewBlock(100, prevHash, currentHash);
+    blockchain.createNewBlock(100, "0", block1Hash);
     expect(blockchain.chain.length).to.be.equal(2);
   });
 
   it("should hash correctly", () => {
-    const block = new Block(0, Date.now(), [], 100, currentHash, prevHash);
+    const block = new Block(0, Date.now(), [], 100, block1Hash, "0");
     const hash = blockchain.hashBlock(block, 100);
     expect(hash).to.not.be.null;
   });
 
   it("should find a legitimate nonce", () => {
-    const block = new Block(0, Date.now(), transactions, 0, currentHash, prevHash);
+    const block = new Block(0, Date.now(), transactions, 0, block1Hash, "0");
     const nonce = blockchain.proofOfWork(block);
     console.log(nonce);
     const hash = blockchain.hashBlock(block, nonce);
     console.log(hash);
   }).timeout(20000);
+
+  it("should return false for an invalid blockchain with invalid hash", () => {
+    const invalidHash = blockchain.isChainValid(invalidTestChainHash);
+    expect(invalidHash).to.be.false;
+  });
+
+  it("should return false for an invalid blockchain with invalid data/transaction", () => {
+    const invalidTransaction = blockchain.isChainValid(invalidTestChainTransaction);
+    expect(invalidTransaction).to.be.false;
+  });
+
+  it("should return false for an invalid blockchain with invalid genesis block", () => {
+    const invalidGenesis = blockchain.isChainValid(invalidTestChainGenesis);
+    expect(invalidGenesis).to.be.false;
+  });
+
+  it("should return true for a valid blockchain", () => {
+    const validChain = blockchain.isChainValid(validTestChain);
+    expect(validChain).to.be.true;
+  });
 });
